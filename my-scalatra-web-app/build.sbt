31a7eacc3dc8cdd1bf56b91f52f6801b89e0f01a
ddwchensf <ddwchensf@gmail.com>
2019-08-01 09:42:47 -0700

    Add json

--- my-scalatra-web-app/build.sbt
+++ my-scalatra-web-app/build.sbt
@@ -17,7 +17,9 @@ libraryDependencies ++= Seq(
   "org.eclipse.jetty" % "jetty-webapp" % "9.4.9.v20180320" % "container",
   "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided",
   "com.google.guava" % "guava" % "28.0-jre",
-  "net.debasishg" %% "redisclient" % "3.10"
+  "net.debasishg" %% "redisclient" % "3.10",
+  "org.scalatra" %% "scalatra-json" % ScalatraVersion,
+  "org.json4s"   %% "json4s-jackson" % "3.5.2"
 )
 
 enablePlugins(SbtTwirl)
--- my-scalatra-web-app/src/main/scala/com/example/app/MyScalatraServlet.scala
+++ my-scalatra-web-app/src/main/scala/com/example/app/MyScalatraServlet.scala
@@ -1,21 +1,47 @@
 package com.example.app
 
 import org.scalatra._
+
 import scala.concurrent.{Await, Future}
 import scala.concurrent.duration._
 import com.redis._
+import org.slf4j.LoggerFactory
+import com.example.app.MyScalatraServlet._
 
-class MyScalatraServlet(redisclient: RedisClient) extends ScalatraServlet {
+// JSON-related libraries
+import org.json4s.{DefaultFormats, Formats}
+
+// JSON handling support from Scalatra
+import org.scalatra.json._
+
+class MyScalatraServlet(redisclient: RedisClient) extends ScalatraServlet with JacksonJsonSupport {
+  protected implicit lazy val jsonFormats: Formats = DefaultFormats
+
+  private val logger = LoggerFactory.getLogger(getClass)
+
+  before() {
+    contentType = formats("json")
+  }
 
   get("/keys/:id") {
-      redisclient.get(params("id")) match {
-        case Some(value) => Ok(value)
-        case None => NotFound()
-      }
+    val key = params("id")
+    redisclient.get(key) match {
+      case Some(value) =>
+        logger.error(s"key:value ${params("id")}:$value")
+        Ok(Pair(Some(key), value))
+      case None => NotFound(s"Cannot find $key")
+    }
   }
 
   put("/keys/:id") {
-    redisclient.set(params("id"), request.body)
-    Ok()
+    val key = params("id")
+    val pair: Pair = parsedBody.extract[Pair]
+    logger.error(s"Request body from curl: $pair")
+    redisclient.set(key, pair.value)
+    Ok(pair.copy(key = Some(key)))
   }
+}
+
+object MyScalatraServlet {
+  case class Pair(key: Option[String], value: String)
 }
