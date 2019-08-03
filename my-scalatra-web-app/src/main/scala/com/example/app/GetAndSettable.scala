062fcb9b0b76f2f614bbd765c12353734d40cc2f
davidchen <ddwchensf@gmail.com>
2019-08-03 12:35:21 -0700

    interface typo

--- my-scalatra-web-app/src/main/scala/com/example/app/GetAndSettable.scala
+++ my-scalatra-web-app/src/main/scala/com/example/app/GetAndSettable.scala
@@ -2,5 +2,5 @@ package com.example.app
 
 trait GetAndSettable {
   def get(key: String): Option[String]
-  def set(key: String, value: String): Unit
+  def set(key: String, value: String): Boolean
 }
--- my-scalatra-web-app/src/main/scala/com/example/app/RedisClientAsGetAndSettable.scala
+++ my-scalatra-web-app/src/main/scala/com/example/app/RedisClientAsGetAndSettable.scala
@@ -4,5 +4,5 @@ import com.redis.RedisClient
 
 class RedisClientAsGetAndSettable(redisClient: RedisClient) extends GetAndSettable {
   override def get(key: String): Option[String] = redisClient.get(key)
-  override def set(key: String, value: String): Unit = redisClient.set(key, value)
+  override def set(key: String, value: String): Boolean = redisClient.set(key, value)
 }
